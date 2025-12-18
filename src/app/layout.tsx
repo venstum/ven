56f8e9295faedd196397faf987811386c11c0f60
venstum <venstum@proton.me>
2025-12-18 14:43:16 -0800

    new public/ files and modfied layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "ven",
   description: "",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
